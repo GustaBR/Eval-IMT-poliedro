5b9a31ca12d4b60d4ca051615d34e3ebadf43fe2
BrunoZaca <brunozacaricalvario@gmail.com>
2025-05-28 11:03:45 -0300

    Tela Rank dividida
    Divisão das classes da tela de ranking em diversas pastas para facilitação do código

---
+++ rank/database.py
@@ -0,0 +1,23 @@
+import mysql.connector  # type: ignore
+from jogador import Jogador
+
+class Database:
+    @staticmethod
+    def carregar_dados():
+        try:
+            conexao = mysql.connector.connect(
+                host="jogomilhao-pi11semestre.l.aivencloud.com",
+                user="avnadmin",
+                port="25159",
+                password="AVNS_2oEh5hU00BYuzgIhTXP",
+                database="jogomilhao"
+            )
+            cursor = conexao.cursor()
+            cursor.execute("SELECT nomeAluno, pontuacao FROM aluno ORDER BY pontuacao DESC LIMIT 100")
+            dados = cursor.fetchall()
+            cursor.close()
+            conexao.close()
+            return [Jogador(nome, pontuacao) for nome, pontuacao in dados]
+        except mysql.connector.Error as err:
+            print("Erro ao conectar ao banco de dados:", err)
+            return []
---
+++ rank/main.py
@@ -0,0 +1,5 @@
+from ranking_app import RankingApp
+
+if __name__ == "__main__":
+    app = RankingApp()
+    app.executar()
